619f0a7f5adf2e5f574907bff270b5508333ea2d
Teeraphan Thienpromthong <pigeonthienpromthong@gmail.com>
2025-03-14 01:54:07 +0700

    connect leaw2

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,20 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import mysql from "mysql2";
+import morgan from "morgan";
 
-const port = 3000;
+// const host = "192.168.1.14";
+const port = 3700;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(morgan("combined")); // Add morgan middleware for logging
 
 const config = {
   user: "root",
   password: "root",
-  host: "localhost",
+  host: "25.32.51.163",
   port: 3400,
   database: "meetingroombooking",
 };
@@ -27,6 +29,11 @@ connection.connect((err) => {
   console.log("Connected to MySQL database");
 });
 
+// Define a route for the root URL
+app.get("/", (req, res) => {
+  res.send("Happy Birthday, Express!");
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
