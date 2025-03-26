03e0f86aeb2bfe875df63b321b92d188052fddc6
ZphinxGZ <zeeyou.ou@gmail.com>
2025-03-26 16:24:10 +0700

    d

---
+++ Backend-SQL/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 3456;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
